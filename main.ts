bf3dc0da4d9f4458e022d7e9d9b353dc3b2ee40d
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2021-01-22 08:19:08 -0500

    Ajout accélération vitesse

--- main.ts
+++ main.ts
@@ -23,21 +23,28 @@ input.onButtonPressed(Button.B, function () {
 let Enemy: game.LedSprite = null
 let Shoot: game.LedSprite = null
 let Ship: game.LedSprite = null
+let Time = 1000
 Ship = game.createSprite(2, 4)
 game.setScore(0)
 basic.forever(function () {
-    Enemy = game.createSprite(randint(0, 4), 0)
-    Enemy.set(LedSpriteProperty.Brightness, 150)
-    basic.pause(100)
-    Enemy.turn(Direction.Right, 90)
-    for (let index = 0; index < 4; index++) {
-        Enemy.move(1)
-        basic.pause(1000)
-        if (Enemy.isTouching(Ship)) {
-            game.gameOver()
+    for (let index = 0; index < 1; index++) {
+        Enemy = game.createSprite(randint(0, 4), 0)
+        Enemy.set(LedSpriteProperty.Brightness, 150)
+        basic.pause(100)
+        Enemy.turn(Direction.Right, 90)
+        for (let index = 0; index < 4; index++) {
+            Enemy.move(1)
+            basic.pause(1000)
+            if (Enemy.isTouching(Ship)) {
+                game.gameOver()
+            }
+        }
+        if (Enemy.isTouchingEdge()) {
+            Enemy.delete()
         }
     }
-    if (Enemy.isTouchingEdge()) {
-        Enemy.delete()
+    Time += -100
+    if (Time <= 100) {
+        Time = 500
     }
 })
